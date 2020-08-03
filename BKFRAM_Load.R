09d7883388e1bf20e01a772e8b1a5f4927c06807
Jon Carey <jonathan.carey@noaa.gov>
2020-08-03 11:14:14 -0700

    BKFRAM_Load.R Update
    1. Modified code to work with and without "SLC_" prefix in RunName when adding Year and BP fields to RunID table
    
    2. Removed dependency on column number in RunID table when assigning runID, year, and bp in main loop
    
    3. Also some random re-indentations

--- BKFRAM_Load.R
+++ BKFRAM_Load.R
@@ -16,6 +16,13 @@
 # 1. Added functionality to deal with RunID tables w/ and w/out RunYear field
 #
 #JC; JUL 2017
+
+# 1. Modified code to work with and without "SLC_" prefix in RunName when adding
+#    Year and BP fields to RunID table
+# 2. Removed dependency on column number in RunID table when assigning runID,
+#    year, and bp in main loop
+#
+#JC; AUG 2020
 ################################################################################
 
 # Clear workspace
@@ -30,20 +37,20 @@ library(readxl)
 library(RODBC)
 
 # Set paths
-Dir <- "C:\\data\\FRAM\\Base Period\\Validation\\BKloadTest\\"
-paths = list(paste(Dir, "Valid2016_FRAM_StockData_7.18.17.xlsm", sep=""),
-             paste(Dir, "2016 Validation_OldBP_Round_5_Prep.mdb", sep=""))
+Dir <- "C:\\Users\\jonathan.carey\\Documents\\FRAM\\Calibration_Validation\\Chinook\\2020\\Abundances\\"
+paths = list(paste(Dir, "Valid2020_FRAM_StockData_7.28.20.xlsm", sep=""),
+             paste(Dir, "Valid2020_BK_junk.mdb", sep=""))
 
 infile = paths[[1]]
 Outfile = paths[[2]]
 
-StartYr = 2010
-EndYr = 2014
+StartYr = 1988
+EndYr = 2018
 
-BP = "Old"
+BP = "New"
 
 # Retain existing flags? 1=Yes, 0=No
-KeepFlags = 1
+KeepFlags = 0
 
 
 #####################################################################
@@ -57,36 +64,36 @@ KeepFlags = 1
 #BK df population
 i=StartYr
 for(i in StartYr:EndYr) {
-    #Does some blackbox wizardry
-    xlApp <- COMCreate("Excel.Application")
-    #xlApp[["Visible"]] <- TRUE
-    #Gets our workbook
-    wb    <- xlApp[["Workbooks"]]$Open(infile)
-    #Gets our FRAMsheet
-    FRAMsheet <- wb$Worksheets("FRAMEscapeV2")
-    
-    # Updates year cell to year i
-    Yearcell <- FRAMsheet$Cells(12,8)
-    Yearcell[["Value"]] <- i
-    
-    # Updates base period cell to BP
-    BPcell <- FRAMsheet$Cells(15,8)
-    BPcell[["Value"]] <- BP
-    
-    wb$Save()
-    
-    xlApp$Quit()
-    
-    # Read in BK data for year i
-    iData <- read_excel(infile, "R_In")
-    
-    # Add BK data for year i into main data table, 'StockData'
-    if(i==StartYr) {
-        BKData <- iData
-    }
-    if(!(i==StartYr)) {
-        BKData <- rbind(BKData, iData)
-    }
+  #Does some blackbox wizardry
+  xlApp <- COMCreate("Excel.Application")
+  #xlApp[["Visible"]] <- TRUE
+  #Gets our workbook
+  wb    <- xlApp[["Workbooks"]]$Open(infile)
+  #Gets our FRAMsheet
+  FRAMsheet <- wb$Worksheets("FRAMEscapeV2")
+  
+  # Updates year cell to year i
+  Yearcell <- FRAMsheet$Cells(12,8)
+  Yearcell[["Value"]] <- i
+  
+  # Updates base period cell to BP
+  BPcell <- FRAMsheet$Cells(15,8)
+  BPcell[["Value"]] <- BP
+  
+  wb$Save()
+  
+  xlApp$Quit()
+  
+  # Read in BK data for year i
+  iData <- read_excel(infile, "R_In")
+  
+  # Add BK data for year i into main data table, 'StockData'
+  if(i==StartYr) {
+    BKData <- iData
+  }
+  if(!(i==StartYr)) {
+    BKData <- rbind(BKData, iData)
+  }
 }
 
 # Pull RunID and BackwardsFRAM tables from Validation Databse
@@ -95,14 +102,14 @@ RunID = sqlQuery(con, as.is = TRUE,
                  paste(sep = '',
                        "SELECT * FROM RunID"))
 BackwardsFRAM = sqlQuery(con, as.is = TRUE,
-                          paste(sep = '',
-                                "SELECT * FROM BackwardsFRAM"))
+                         paste(sep = '',
+                               "SELECT * FROM BackwardsFRAM"))
 close(con)
 
 # Sort by RunID and add year and BasePeriod fields
 RunID <- RunID[order(RunID$RunID), ]
-RunID$Year <- substr(RunID$RunName,11,14)
-RunID$BP <- substr(RunID$RunName,16,18)
+RunID$Year <- ifelse(substr(RunID$RunName,1,3) == "SLC",substr(RunID$RunName,15,18),substr(RunID$RunName,11,14))
+RunID$BP <- ifelse(substr(RunID$RunName,1,3) == "SLC",substr(RunID$RunName,20,22),substr(RunID$RunName,16,18))
 
 # Get existing BK flags
 BKflags <- BackwardsFRAM[ ,c(1,2,6)]
@@ -110,67 +117,61 @@ BKflags <- BackwardsFRAM[ ,c(1,2,6)]
 # Append BK data to BackwardsFRAM table
 i=StartYr
 for (i in StartYr:EndYr) {
-    if(dim(RunID)[2] == 12) { # for RunID table that does not contain RunYear field
-        runID <- RunID[RunID$Year == i, 2]
-        year <- RunID[RunID$Year == i, 11]
-        bp <- RunID[RunID$Year == i, 12]
+  RunID_temp <- RunID[RunID$Year == i,]
+  runID <- RunID_temp$RunID
+  year <- RunID_temp$Year
+  bp <- RunID_temp$BP
+  rm(RunID_temp)
+
+  if(bp == "Old") {
+    BKFRAM <- subset(BKData, YEAR == year & StockID <114)
+    BKFRAM$RunID <- c(rep(runID, dim(BKFRAM)[1]))
+    BKFRAM <- BKFRAM[ ,c(7,2:6)]
+    BKFRAM <- format(BKFRAM, scientific = FALSE)
+    BKFRAM$RunID <- as.integer(BKFRAM$RunID)
+    BKFRAM$StockID <- as.integer(BKFRAM$StockID)
+    
+    if(KeepFlags == 1) {
+      bkflags <- subset(BKflags, BKflags$RunID == runID)
+      colnames(BKFRAM)[6] <- "BadFlags"
+      BKFRAM <- merge(BKFRAM,bkflags)
+      BKFRAM <- BKFRAM[ ,c(1:5,7)]
     }
-    if(dim(RunID)[2] == 14) { # for RunID table that does contain RunYear field
-        runID <- RunID[RunID$Year == i, 2]
-        year <- RunID[RunID$Year == i, 13]
-        bp <- RunID[RunID$Year == i, 14]
+  }
+  
+  if(bp == "New") {
+    BKFRAM <- subset(BKData, YEAR == year)
+    BKFRAM$RunID <- c(rep(runID, dim(BKFRAM)[1]))
+    BKFRAM <- BKFRAM[ ,c(7,2:6)]
+    BKFRAM$TargetEscAge3 <- format(BKFRAM$TargetEscAge3, scientific = FALSE)
+    BKFRAM$TargetEscAge4 <- format(BKFRAM$TargetEscAge4, scientific = FALSE)
+    BKFRAM$TargetEscAge5 <- format(BKFRAM$TargetEscAge5, scientific = FALSE)
+    BKFRAM$RunID <- as.integer(BKFRAM$RunID)
+    BKFRAM$StockID <- as.integer(BKFRAM$StockID)
+    
+    if(KeepFlags == 1) {
+      bkflags <- subset(BKflags, BKflags$RunID == runID)
+      colnames(BKFRAM)[6] <- "BadFlags"
+      BKFRAM <- merge(BKFRAM,bkflags)
+      BKFRAM <- BKFRAM[ ,c(1:5,7)]
     }
-    
-    
-    if(bp == "Old") {
-        BKFRAM <- subset(BKData, YEAR == year & StockID <114)
-        BKFRAM$RunID <- c(rep(runID, dim(BKFRAM)[1]))
-        BKFRAM <- BKFRAM[ ,c(7,2:6)]
-        BKFRAM <- format(BKFRAM, scientific = FALSE)
-        BKFRAM$RunID <- as.integer(BKFRAM$RunID)
-        BKFRAM$StockID <- as.integer(BKFRAM$StockID)
-        
-        if(KeepFlags == 1) {
-            bkflags <- subset(BKflags, BKflags$RunID == runID)
-            colnames(BKFRAM)[6] <- "BadFlags"
-            BKFRAM <- merge(BKFRAM,bkflags)
-            BKFRAM <- BKFRAM[ ,c(1:5,7)]
-        }
-    }
-    
-    if(bp == "New") {
-        BKFRAM <- subset(BKData, YEAR == year)
-        BKFRAM$RunID <- c(rep(runID, dim(BKFRAM)[1]))
-        BKFRAM <- BKFRAM[ ,c(7,2:6)]
-        BKFRAM$TargetEscAge3 <- format(BKFRAM$TargetEscAge3, scientific = FALSE)
-        BKFRAM$TargetEscAge4 <- format(BKFRAM$TargetEscAge4, scientific = FALSE)
-        BKFRAM$TargetEscAge5 <- format(BKFRAM$TargetEscAge5, scientific = FALSE)
-        BKFRAM$RunID <- as.integer(BKFRAM$RunID)
-        BKFRAM$StockID <- as.integer(BKFRAM$StockID)
-        
-        if(KeepFlags == 1) {
-            bkflags <- subset(BKflags, BKflags$RunID == runID)
-            colnames(BKFRAM)[6] <- "BadFlags"
-            BKFRAM <- merge(BKFRAM,bkflags)
-            BKFRAM <- BKFRAM[ ,c(1:5,7)]
-        }
-    }
-    
-    # UPDATE DATABASE
-    con = odbcConnectAccess(Outfile)
-    
-    # First delete records in BackwardsFRAM table for RunID of year 'i'
-    sqlQuery(con, as.is = TRUE,
-             paste(sep = '',
-                   "DELETE BackwardsFRAM.RunID ",
-                   "FROM BackwardsFRAM ",
-                   "WHERE (((BackwardsFRAM.RunID)=",runID,"))"))
-    
-    # Next append new BKFRAM abundances to BackwardsFRAM table for RunID of year 'i'
-    sqlSave(con, BKFRAM, tablename = "BackwardsFRAM", rownames = FALSE,
-            append = TRUE)
-    
-    close(con)
+  }
+  
+  # UPDATE DATABASE
+  con = odbcConnectAccess(Outfile)
+  
+  # First delete records in BackwardsFRAM table for RunID of year 'i'
+  sqlQuery(con, as.is = TRUE,
+           paste(sep = '',
+                 "DELETE BackwardsFRAM.RunID ",
+                 "FROM BackwardsFRAM ",
+                 "WHERE (((BackwardsFRAM.RunID)=",runID,"))"))
+  
+  # Next append new BKFRAM abundances to BackwardsFRAM table for RunID of year 'i'
+  sqlSave(con, BKFRAM, tablename = "BackwardsFRAM", rownames = FALSE,
+          append = TRUE)
+  
+  close(con)
 }
 
 # i=1
