fb068e5c6b16634718c45e441157042a606ae40f
Jon Carey <jonathan.carey@noaa.gov>
2020-10-15 11:48:01 -0700

    Add FRAMBuilder CWT comparison
    Adding code and support files for shiny apps that compare raw CWT recoveries between FRAMBuilder runs.

---
+++ FRAMBuilder_CWT_Comparison/FRAMBuilder_CWTs_by_Fishery/app.R
@@ -0,0 +1,80 @@
+library(shiny)
+library(plotly)
+library(tidyr)
+library(dplyr)
+
+CWT_data <- read.csv("FRAMBuilder_CWT_comparison.csv")
+Stk_LUT <- read.csv("StockLUT.csv", stringsAsFactors = FALSE)
+Fish_LUT <- read.csv("FishLUT.csv", stringsAsFactors = FALSE)
+CWT_data$StockNum <- CWT_data$StockNum /2
+CWT_data <- merge(CWT_data, Stk_LUT)
+CWT_data$StockTitle <- factor(CWT_data$StockTitle, levels = c(Stk_LUT$StockTitle), ordered = TRUE)
+CWT_data$FisheryName <- factor(CWT_data$FisheryName, levels = c(Fish_LUT$FisheryTitle), ordered = TRUE)
+colnames(CWT_data)[13:14] <- c("Expanded", "Nominal")
+CWT_data_long <- CWT_data %>% 
+  gather(RecType, Recs, 13:14) %>%
+  filter(Notes == "") %>%
+  group_by(Version, StockTitle, FisheryName, RecType) %>%
+  summarize(Recoveries = sum(Recs))
+
+# Define UI ----
+ui <- fluidPage(
+
+  # App title ----
+  titlePanel("FRAMBuilder CWTs by Fishery"),
+
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+
+    # Sidebar panel for inputs ----
+    sidebarPanel(width = 8,
+
+      # Input: stock selector ----
+      selectInput(inputId = "fishery",
+                  label = "Select fishery:",
+                  choices = as.character(unique(Fish_LUT$FisheryTitle)))
+
+    ),
+
+    # Main panel for displaying outputs ----
+    mainPanel(
+
+      # Output: 
+      plotlyOutput(outputId = "Plot", width = "120%", height = "600px")
+
+    )
+  )
+)
+
+# Define server ----
+server <- function(input, output) {
+  
+  CWT_stock <- reactive({
+    CWT_data_long %>%
+      filter(FisheryName == input$fishery)
+  })
+  
+  output$Plot <- renderPlotly({
+
+    print(
+      ggplotly(
+        ggplot(data = CWT_stock(), aes(StockTitle, Recoveries, fill = Version)) +
+          geom_bar(width=0.7, size=0.2, color = "black", alpha=1, position="dodge", stat="identity") +
+          scale_fill_manual(values = c("#66CDAA","#6CA6CD")) +
+          facet_grid(RecType ~ ., scales = "free_y") +
+          ggtitle(paste(input$fishery)) +
+          xlab("") +
+          ylab("Recoveries") +
+          theme_bw() +
+          theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+          theme(legend.position = "top", legend.title = element_blank())
+      )
+    ) %>%
+      layout(legend = list(orientation = "h", x = 0.8, y = 1.1))
+    
+    })
+
+}
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
