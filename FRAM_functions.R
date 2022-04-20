9a7a8a70770bfd8100b6a44da59d981c9410a52a
Jon Carey <jonathan.carey@noaa.gov>
2022-04-19 20:32:34 -0700

    Update FRAM_functions.R
    1. Add pull_BackwardsFRAM()
    2. Add pull_FisheryScalers()
    3. Fix ZeroPS_SRKW_2021() so it doesn't accidentally update other runIDs
    4. Misc cleanup

--- FRAM_functions.R
+++ FRAM_functions.R
@@ -6,51 +6,59 @@
 #       pulls AEQ table from a specified database, 
 #       option to specify bpID
 #
-# 2. pull_Cohort(db_path, runID, stock, age, timestep): 
+# 2. pull_BackwardsFRAM(db_path, runID): 
+#       pulls BackwardsFRAM table from a specified database, 
+#       option to specify runID
+#
+# 3. pull_Cohort(db_path, runID, stock, age, timestep): 
 #       pulls Cohort table from a specified database, 
 #       option to specify runID, stock, age, timestep
 #
-# 3. pull_Escapement(db_path, runID, stock, age, timestep): 
+# 4. pull_Escapement(db_path, runID, stock, age, timestep): 
 #       pulls Escapement table from a specified database, 
 #       option to specify runID, stock, age, timestep
 #
-# 4. pull_MaturationRate(db_path, bpID): 
+# 5. pull_FisheryScalers(db_path, runID, fishery, timestep): 
+#       pulls pull_FisheryScalers table from a specified database, 
+#       option to specify runID, fishery, timestep
+#
+# 6. pull_MaturationRate(db_path, bpID): 
 #       pulls MaturationRate table from a specified database, 
 #       option to specify bpID
 #
-# 5. pull_Mortality(db_path, runID, stock, age, fishery, timestep): 
+# 7. pull_Mortality(db_path, runID, stock, age, fishery, timestep): 
 #       pulls Mortality table from a specified database, 
 #       option to specify runID, stock, age, fishery, timestep
 #
-# 6. pull_RunID(db_path, runID): 
+# 8. pull_RunID(db_path, runID): 
 #       pulls RunID table from a specified database, 
 #       option to specify runID, stock, age, timestep
 #
-# 7. pull_TerminalFisheryFlag(db_path, bpID): 
+# 9. pull_TerminalFisheryFlag(db_path, bpID): 
 #       pulls TerminalFisheryFlag table from a 
 #       specified database, option to specify bpID
 #
-# 8. ZeroPS_SRKW(db_path, runID): 
+# 10. ZeroPS_SRKW(db_path, runID): 
 #       zeros out all PS fishery and CNR inputs (with exception of 
 #       Hood Canal sport & net and 13A net), updates remaining ISBM
 #       fishery flags to scalers. Can accomodate multiple runIDs.
 #
-# 9. ZeroPS_SRKW_2021(db_path, runID): 
+# 11. ZeroPS_SRKW_2021(db_path, runID): 
 #       zeros out all PS fishery and CNR inputs for fisheries relevant
 #       to SRKW analyses for PS fisheries, based on 2021 discussions 
 #       with PS comanagers (see 'FRAM Fishery Exclusions_rev9.30.21.xlsx').
 #       updates remaining ISBM fishery flags to scalers. 
 #       Can accomodate multiple runIDs.
 #
-# 10. ZeroPS(db_path, runID): 
+# 12. ZeroPS(db_path, runID): 
 #       zeros out all PS fishery and CNR inputs, updates remaining 
 #       ISBM fishery flags to scalers. Can accomodate multiple runIDs.
 #
-# 11. calc_SRFI(db_path, runID, SRFI_BP_ER, outfile): 
+# 13. calc_SRFI(db_path, runID, SRFI_BP_ER, outfile): 
 #       calculates SRFI values for a supplied list of RunIDs; requires a
 #       SRFI_BP_ER to be supplied, will output a csv to outfile.
 #
-# 12. calc_SRFI_BP_ER(db_path):
+# 14. calc_SRFI_BP_ER(db_path):
 #       calculates the SRFI base period ER (1988-1993) for the denominator
 #       in the SRFI calculation. Requires 'db_path' that refers to the
 #       relevant validation database. Returns a single value.
@@ -81,6 +89,30 @@ pull_AEQ <- function(db_path, bpID) {
   
   close(con)
   return(AEQ[order(AEQ$BasePeriodID,AEQ$StockID,AEQ$Age,AEQ$TimeStep), ])
+}
+#----------------------------------------------------------------------------#
+
+
+#----------------------------------------------------------------------------#
+# Function to pull BackwardsFRAM table from a FRAM database
+pull_BackwardsFRAM <- function(db_path, runID) {
+  con = odbcConnectAccess(db_path)
+  
+  if(missing(runID)) {
+    bkFRAM = sqlQuery(con, as.is = TRUE,
+                     paste(sep = '',
+                           "SELECT * FROM BackwardsFRAM"))
+  } else {
+    runID_string <- toString(sprintf("%s", runID))
+    bkFRAM = sqlQuery(con, as.is = TRUE,
+                     paste(sep = '',
+                           "SELECT BackwardsFRAM.* ",
+                           "FROM BackwardsFRAM ",
+                           "WHERE (((BackwardsFRAM.RunID) In (",runID_string,")));"))
+  }
+  
+  close(con)
+  return(bkFRAM[order(bkFRAM$RunID), ])
 }
 #----------------------------------------------------------------------------#
 
@@ -203,6 +235,64 @@ pull_Escapement <- function(db_path, runID=NULL, stock=NULL, age=NULL, timestep=
   close(con)
   
   return(Escapement[order(Escapement$RunID,Escapement$StockID,Escapement$Age,Escapement$TimeStep), ])
+}
+#----------------------------------------------------------------------------#
+
+
+#----------------------------------------------------------------------------#
+# Function to pull FisheryScalers table from a FRAM database
+pull_FisheryScalers <- function(db_path, runID=NULL, fishery=NULL, timestep=NULL) {
+  x <- list()
+  if(is.null(runID) == FALSE) {
+    x[["RunID"]] <- runID
+  }
+  if(is.null(fishery) == FALSE) {
+    x[["FisheryID"]] <- fishery
+  }
+  if(is.null(timestep) == FALSE) {
+    x[["TimeStep"]] <- timestep
+  }
+  
+  con = odbcConnectAccess(db_path)
+  if(length(x) == 0) {
+    FishScalers <- sqlQuery(con, as.is = TRUE,
+                     paste(sep = '',
+                           "SELECT * FROM FisheryScalers"))
+  }
+  
+  if(length(x) == 1) {
+    x1_string <- toString(sprintf("%s", x[[1]]))
+    FishScalers = sqlQuery(con, as.is = TRUE,
+                    paste(sep = '',
+                          "SELECT FisheryScalers.* ",
+                          "FROM FisheryScalers ",
+                          "WHERE (((FisheryScalers.",names(x[1]),") In (",x1_string,")));"))
+  }
+  
+  if(length(x) > 1) {
+    x1_string <- toString(sprintf("%s", x[[1]]))
+    where_clause <- paste("WHERE (((FisheryScalers.",names(x[1]),") In (",x1_string,"))",sep = "")
+    
+    for(i in 2:length(x)) {
+      xi_string <- toString(sprintf("%s", x[[i]]))
+      if(i == 2) {
+        and_clause <- paste(" AND ((FisheryScalers.",names(x[i]),") In (",xi_string,"))",sep = "")
+      }
+      if(i > 2) {
+        and_clause <- paste(and_clause," AND ((FisheryScalers.",names(x[i]),") In (",xi_string,"))",sep = "")
+      }
+    }
+    
+    FishScalers = sqlQuery(con, as.is = TRUE,
+                    paste(sep = '',
+                          "SELECT FisheryScalers.* ",
+                          "FROM FisheryScalers ",
+                          where_clause,
+                          and_clause,");"))
+  }
+  close(con)
+  
+  return(FishScalers[order(FishScalers$RunID,FishScalers$FisheryID,FishScalers$TimeStep), ])
 }
 #----------------------------------------------------------------------------#
 
@@ -372,7 +462,7 @@ ZeroPS_SRKW <- function(db_path, runID) {
                  "UPDATE FisheryScalers SET FisheryScalers.FisheryFlag = 7 ",
                  "WHERE (((FisheryScalers.RunID) In (",runID_string,")) ",
                  "AND (((FisheryScalers.FisheryID) In (4,5,6,7,12,13,14,15,16,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,64,65,66,70,71)) ",
-                 "OR ((FisheryScalers.FisheryID)=17 And (FisheryScalers.TimeStep) In (2,3))) AND ((FisheryScalers.FisheryFlag)=8));"))
+                 "OR ((FisheryScalers.FisheryID)=17 AND (FisheryScalers.TimeStep) In (2,3))) AND ((FisheryScalers.FisheryFlag)=8));"))
   
   # query to update remaining ISBM flags from 28 to 17
   sqlQuery(con, as.is = TRUE,
@@ -380,13 +470,13 @@ ZeroPS_SRKW <- function(db_path, runID) {
                  "UPDATE FisheryScalers SET FisheryScalers.FisheryFlag = 17 ",
                  "WHERE (((FisheryScalers.RunID) In (",runID_string,")) ",
                  "AND (((FisheryScalers.FisheryID) In (4,5,6,7,12,13,14,15,16,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,64,65,66,70,71)) ",
-                 "OR ((FisheryScalers.FisheryID)=17 And (FisheryScalers.TimeStep) In (2,3))) AND ((FisheryScalers.FisheryFlag)=28));"))
+                 "OR ((FisheryScalers.FisheryID)=17 AND (FisheryScalers.TimeStep) In (2,3))) AND ((FisheryScalers.FisheryFlag)=28));"))
   
   # query to zero out non-retention in PS fisheries
   sqlQuery(con, as.is = TRUE,
            paste(sep = '',
                  "UPDATE NonRetention SET NonRetention.CNRInput1 = 0, NonRetention.CNRInput2 = 0, NonRetention.CNRInput3 = 0, NonRetention.CNRInput4 = 0 ",
-                 "WHERE (((NonRetention.RunID) In (",runID_string,")) AND ((NonRetention.FisheryID)>=36 And (NonRetention.FisheryID) Not In (64,65,66,70,71)));"))
+                 "WHERE (((NonRetention.RunID) In (",runID_string,")) AND ((NonRetention.FisheryID)>=36 AND (NonRetention.FisheryID) Not In (64,65,66,70,71)));"))
   
   close(con)
 }
@@ -432,14 +522,14 @@ ZeroPS_SRKW_2021 <- function(db_path, runID) {
                  "UPDATE NonRetention SET NonRetention.CNRInput1 = 0, NonRetention.CNRInput2 = 0, NonRetention.CNRInput3 = 0, NonRetention.CNRInput4 = 0 ",
                  "WHERE (((NonRetention.RunID) In (",runID_string,")) AND ((NonRetention.FisheryID)>=36 And (NonRetention.FisheryID) Not In (48,51,52,60,61,62,63,64,65,66,68,69,70,71)));"))
   
-  # query to zero out PS fisheries (with exception of 'A 12 Sport', 'NT HC Net', 'Tr HC Net', 'Tr HC Net', and 'Tr 13A Net')
+  # query to zero out PS fisheries (with exception of terminal fisheries identified for exclusion from analysis - see "FRAM Fishery Exclusions_rev9.30.21.xlsx")
   sqlQuery(con, as.is = TRUE,
            paste(sep = '',
                  "UPDATE FisheryScalers SET FisheryScalers.FisheryFlag = 2, FisheryScalers.Quota = 0 ",
                  "WHERE ((FisheryScalers.RunID) In (",runID_string,") ",
                  "AND ((FisheryScalers.TimeStep) In (1,4) AND ((FisheryScalers.FisheryID)=17 OR (FisheryScalers.FisheryID)>=36)) ",
-                 "OR ((FisheryScalers.TimeStep)=2 AND ((FisheryScalers.FisheryID)>=36 AND (FisheryScalers.FisheryID) Not In (48))) ",
-                 "OR ((FisheryScalers.TimeStep)=3 AND ((FisheryScalers.FisheryID)>=36 AND (FisheryScalers.FisheryID) Not In (48,51,52,60,61,62,63,64,65,66,68,69,70,71))));"))
+                 "OR ((FisheryScalers.RunID) In (",runID_string,") AND (FisheryScalers.TimeStep)=2 AND ((FisheryScalers.FisheryID)>=36 AND (FisheryScalers.FisheryID) Not In (48))) ",
+                 "OR ((FisheryScalers.RunID) In (",runID_string,") AND (FisheryScalers.TimeStep)=3 AND ((FisheryScalers.FisheryID)>=36 AND (FisheryScalers.FisheryID) Not In (48,51,52,60,61,62,63,64,65,66,68,69,70,71))));"))
   
   close(con)
 }
@@ -555,13 +645,13 @@ calc_SRFI <- function(db_path, runID, SRFI_BP_ER, outfile) {
 
 #----------------------------------------------------------------------------#
 # Function to calculate SRFI base period ER (1988-1993)
-# Requires: db_path, series of RunIDs, SRFI_BP_ER, and an outfile path
+# Requires: db_path
 
 calc_SRFI_BP_ER <- function(db_path) {
   run_IDs <- pull_RunID(db_path)
   run_IDs <- run_IDs[run_IDs$RunYear >= 1988 & run_IDs$RunYear <= 1993, c(2,4,6,11)]
   
-  SRFI_log <- data.frame(matrix(ncol = 3, nrow = length(run_IDs)))
+  SRFI_log <- data.frame(matrix(ncol = 3, nrow = dim(run_IDs)[1]))
   colnames(SRFI_log) <- c("RunID", "Year", "SRFI_ER")
   
   for(i in 1:dim(run_IDs)[1]) {
